78ef0c0eb67bdef4e549598ae284c785b841d3ae
Jan Juroszek <janko.juroszek@gmail.com>
2026-04-29 08:56:01 +0200

    the idea is here, i just have to uncover it
    Change-Id: I0ac53635a9c2e087f2669fdb55519be82109d3db

--- gcd/core/config.py
+++ gcd/core/config.py
@@ -2,8 +2,8 @@ import os
 import tomllib
 from pathlib import Path
 
-from gcd.core.models import GerritInstance
 from gcd.core.logs import app_logger
+from gcd.core.models import GerritInstance
 
 DEFAULT_INTERVAL = 30
 DEFAULT_REFRESH_RATE = 20
@@ -114,7 +114,7 @@ class AppConfig:
             host = ins.get("host") or default_host
             port = ins.get("port") or default_port
             email = ins.get("email") or default_email
-            enabled_plugins = ins.get("plugins_enabled") or default_plugins_enabled
+            enabled_plugins = ins.get("plugins_enabled", []) + default_plugins_enabled
 
             if enabled_plugins and not isinstance(enabled_plugins, list):
                 raise ValueError(f"plugins_enabled for instance {ins_name} must be a list")
